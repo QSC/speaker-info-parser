79b9d1d0a01f3a9ece8014d9af50c7192e7df841
Incandescent-Turtle <rorsmacd@gmail.com>
2023-12-26 15:29:15 -0800

    set up env variables

--- parser.js
+++ parser.js
@@ -1,23 +1,17 @@
+require('dotenv').config();
 const axios = require('axios');
-const fs = require('fs'); // Node.js file system module
 
-const spreadsheetId = 'YOUR_SPREADSHEET_ID';
-const sheetId = 'YOUR_SHEET_ID';
-const apiKey = 'YOUR_API_KEY'; // If using an API key
+const apiKey = process.env.API_KEY;
+const sheetId = process.env.SHEET_ID;
+const sheetRange = 'Sheet1';
 
-// Construct the URL to retrieve the sheet in CSV format
-const url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/export?format=csv&id=${spreadsheetId}&gid=${sheetId}&key=${apiKey}`;
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetRange}?key=${apiKey}&alt=csv`;
 
-// Make a GET request to fetch the CSV data
 axios.get(url)
 	.then(response => {
-		if (response.status !== 200) {
-			throw new Error('Failed to fetch data');
-		}
-		// Write the CSV data to a file
-		fs.writeFileSync('data.csv', response.data);
-		console.log('CSV data has been saved to data.csv');
-	})
-	.catch(error => {
-		console.error('There was a problem with the fetch operation:', error);
+		// Handle the response data
+		console.log(response.data);
+	}).catch(error => {
+		// Handle errors
+		console.error('There was an error fetching the data:', error);
 	});
