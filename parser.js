499b9b1c2ae25467242eb6d98571d40398885569
Incandescent-Turtle <rorsmacd@gmail.com>
2023-12-26 17:54:36 -0800

    remove leading/trailing whitespace
    - removed extra whitespace from string enteries on all speaker objects

--- parser.js
+++ parser.js
@@ -8,8 +8,7 @@ fs.createReadStream('speaker_data.csv')
 	.on('data', (data) => results.push(data))
 	.on('end', () => {
 
-		console.log(results)
-
+		// take out empty lines
 		results = results.filter(speaker =>
 		{
 			let empty = true
@@ -23,16 +22,20 @@ fs.createReadStream('speaker_data.csv')
 			return !empty
 		})
 
+		// formatted into arrays for each year
 		const formattedResults = {}
 		for (const speaker of results)
 		{
+			// trim leading/trailing whitespace on string fields
+			Object.keys(speaker).forEach(k => speaker[k] = typeof speaker[k] == 'string' ? speaker[k].trim() : speaker[k]);
+
 			const year = speaker.year
 			if (!formattedResults[year])
 			{
 				formattedResults[year] = []
-				delete speaker.year
-				formattedResults[year].push(speaker)
 			}
+			delete speaker.year
+			formattedResults[year].push(speaker)
 		}
 
 		const jsonData = JSON.stringify(formattedResults, null, 2);
